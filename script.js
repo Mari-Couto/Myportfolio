5477ecaa62d220c90cba6c683e0c16df404af12b
Marina Couto <marinacoutonina@gmail.com>
2023-07-03 17:01:20 -0300

    scrollButton

--- script.js
+++ script.js
@@ -25,3 +25,20 @@ cards.forEach(card => {
     card.style.borderColor = '#ccc';
   });
 });
+
+//Botão de voltar
+window.addEventListener("scroll", function() {
+  let scrollButton = document.querySelector(".voltar");
+  if (document.documentElement.scrollTop > 30) {
+    scrollButton.style.display = "block";
+  } else {
+    scrollButton.style.display = "none";
+  }
+});
+
+document.querySelector(".voltar").addEventListener("click", function() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+});

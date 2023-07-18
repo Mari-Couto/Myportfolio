0e158cbd001d6178385ce8c93fe15453f2753503
Marina Couto <marinacoutonina@gmail.com>
2023-07-18 17:56:58 -0300

    responsividade dropdown

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ const dropDownMenu = document.querySelector('.dropdown_menu')
 toggleBtn.onclick = function () {
     dropDownMenu.classList.toggle('open')
     const isOpen = dropDownMenu.classList.contains('open')
+
     
     toggleBtnIcon.classList = isOpen
     ? 'fa-solid fa-xmark'
